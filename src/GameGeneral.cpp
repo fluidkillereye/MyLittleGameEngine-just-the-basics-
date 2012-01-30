f1b9ba4b828ca4789963c9120efb63ec957cf291
Johannes Smit <johannessmit@live.nl>
2012-01-30 21:50:14 +0100

    Folders for the .cpp/.h files

---
+++ src/GameGeneral.cpp
@@ -0,0 +1,11 @@
+#include "include/GameGeneral.h"
+
+GameGeneral::GameGeneral()
+{
+
+}
+
+GameGeneral::~GameGeneral()
+{
+
+}
---
+++ src/include/GameGeneral.h
@@ -0,0 +1,14 @@
+#ifndef GAMEGENERAL_H
+#define GAMEGENERAL_H
+
+
+class GameGeneral
+{
+    public:
+        GameGeneral();
+        virtual ~GameGeneral();
+    protected:
+    private:
+};
+
+#endif // GAMEGENERAL_H
